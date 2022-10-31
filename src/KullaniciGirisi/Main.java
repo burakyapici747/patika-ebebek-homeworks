548249610b1d67493400ff2e305738399f31b76a
Burak YAPICI <burakyapici747@gmail.com>
2022-11-01 00:37:23 +0300

    Paratik - Kullanici Girisi

--- src/KullaniciGirisi/Main.java
+++ src/KullaniciGirisi/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         String username, password, choice, newPassword ;
         final Scanner scanner = new Scanner(System.in);
 
