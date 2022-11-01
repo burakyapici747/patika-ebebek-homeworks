de21132215b51266050d8266f919f0f4d6d5472e
Burak YAPICI <burakyapici747@gmail.com>
2022-11-01 16:39:26 +0300

    Paratik - Dizideki en buyuk ve en kucuk en yakin

---
+++ src/DiziEnKucukEnYakinveEnBuyukEnYakinBulma/Main.java
@@ -0,0 +1,42 @@
+package DiziEnKucukEnYakinveEnBuyukEnYakinBulma;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final Scanner scanner = new Scanner(System.in);
+        int[] list = {15, 12, 788, 1, -1, -778, 2, 0};
+
+        System.out.println(Arrays.toString(list));
+
+        for (int item : list){
+            System.out.print(item);
+            System.out.print(" , ");
+        }
+
+        Arrays.sort(list);
+
+        System.out.println();
+        System.out.print("Sayi giriniz:  ");
+        int nearNumber = scanner.nextInt();
+
+        int minvalue = list[0];
+        int maxvalue = list[0];
+
+        for (int i : list){
+            if (i < nearNumber){
+                minvalue = i;
+            }
+            if (i > nearNumber){
+                maxvalue = i;
+                break;
+            }
+        }
+
+        System.out.println("Girilen sayıya en yakın en küçük eleman:  " + minvalue);
+        System.out.println("Girilen sayıya en yakın en büyük eleman:  " + maxvalue);
+    }
+}
